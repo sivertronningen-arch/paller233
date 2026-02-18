80ebddd229aca0e2978c99cbba79aed6885d550e
sivertronningen-arch <sivert.ronningen@gmail.com>
2026-02-18 17:04:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -362,4 +362,4 @@ def static_files(filename):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", "5000")), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", "5000")), debug=False)
